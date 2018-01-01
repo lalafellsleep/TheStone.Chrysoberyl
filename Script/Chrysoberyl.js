7ef8ca89b2f15d5b26f6e0e6cf8eba74e6deffef
Laighlinne <mylastmogletter@gmail.com>
2018-01-02 00:58:11 +0900

    Create Chrysoberyl.js

---
+++ Script/Chrysoberyl.js
@@ -0,0 +1,18 @@
+var Chrysoberyl = new function()
+{
+    // set Overlay Types
+    this.overlayTypes = {
+        "Browser":0, // ACTWebSocket with Browser
+        "OverlayPlugin":1, // OverlayPlugin
+        "OverlayProcess":2, // ACTWebSocket
+    };
+    
+    // attach event
+    this.getACTData = function()
+    {
+    
+    };
+    
+    // 
+    this.getACTOverlayType = overlayTypes.Browser;
+};
